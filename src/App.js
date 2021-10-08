7a700bd18eb694e77537ef2ae36adb0c0aa9ce62
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-10-08 15:50:05 +0900

    5.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
