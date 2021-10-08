1b13332f00233fd63b2ec03277461b0032461f8c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-10-08 17:42:00 +0900

    6.1 To Do List part Two

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
     setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
   };
+  console.log(toDos);
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -24,6 +26,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
